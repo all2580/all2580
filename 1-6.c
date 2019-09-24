dcd0aa170903faef0d68f0ef1cfa4f147a7b64b1
all2580 <55266816+all2580@users.noreply.github.com>
2019-09-24 19:15:10 +0600

    Create 1-6.c

---
+++ 1-6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  int i =1;
+  int count =1; 
+  scanf("%d",&n);
+
+  int temp =n;
+  while (temp >=10){
+    temp/=10;
+    i*=10;
+  }
+  
+  while (n>=10){
+    if(n%10 !=n/i){
+      count=0;
+      break;
+      
+    }
+    n=(n%i)/10;
+    i/=100;
+  }
+
+  if (count==1){
+    printf ("N is palindrom");
+  }
+  else{
+    printf ("N is not palindrom");
+  }
+
+  return 0;
+}
