6faf4036dee29c82774645ca79e7e42849c5b3ff
all2580 <55266816+all2580@users.noreply.github.com>
2019-10-01 15:50:15 +0600

    Create 2-3.c

---
+++ 2-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+double power(double x, int n) {
+  
+  if (n>0){
+    return x * power(x,n-1);
+  } else {
+    return 1;
+  }
+  
+}
+
+int main(void) {
+  double x;
+  int n;
+  scanf("%lf %d", &x, &n);
+
+  printf("%lf", power(x, n));
+  return 0;
+}
