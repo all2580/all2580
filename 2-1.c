c32ea7f9f1e1dc98488bcfc3fa184e5238fe89b9
all2580 <55266816+all2580@users.noreply.github.com>
2019-10-25 11:47:26 +0700

    Update 2-1.c

--- 2-1.c
+++ 2-1.c
@@ -2,28 +2,20 @@
 
 unsigned char invert(unsigned char x, unsigned char p, unsigned char n) {
 
-  printf(" %hhu", x);
+  //printf(" %hhu", x);
 
-  if (p > 7) {
+  if (p > 7 || n <= 0)
     return x;
-  } else if (n > 0) {
-    x = x ^ (1 << (p));
-    n--;
-    p++;
-    return invert(x, p, n);
-  } else {
-    return x;
-  }
+  
+  unsigned char new_x = x ^ (1 << p);
+  return invert(new_x, p+1, n-1);
 }
-
 int main(void) {
-  unsigned char x;
-  unsigned char p;
-  unsigned char n;
+  unsigned char x, p, n;
+ 
   scanf("%hhu %hhu %hhu", &x, &p, &n);
 
   printf("  %hhu ", invert(x, p, n));
 
   return 0;
 }
-
