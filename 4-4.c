aca999fe742a19d9d5f97b6d5751f36d4a6d19a5
AngryPussy <55266816+all2580@users.noreply.github.com>
2019-11-06 00:59:36 +0600

    Update 4-4.c

--- 4-4.c
+++ 4-4.c
@@ -8,44 +8,45 @@ int str_len(const char word1[]) {
   return len;
 }
 
-const char* _strstr(const char *str, const char *substr) {
+const char *_strstr(const char *str, const char *substr) {
   int len = str_len(str);
   int sub_len = str_len(substr);
-  const char* ptr=NULL;
+  const char *ptr = NULL;
 
-  for (int i=0; i<len; i++){
-    ptr= &str[i];
-    
-    for (int j=0;j< sub_len;j++){
-      if (str[i+j]!= substr[j]){
-        ptr=NULL;
+  for (int i = 0; i < len; i++) {
+    ptr = &str[i];
+
+    for (int j = 0; j < sub_len; j++) {
+      if (str[i + j] != substr[j]) {
+        ptr = NULL;
         break;
       }
     }
 
-    if(ptr==(char*)&str[i]){
+    if (ptr == (char *)&str[i]) {
       break;
     }
   }
- 
+
   return ptr;
 }
 
-void find_str(char const* str, char const* substr) {
-    const char* pos = _strstr(str, substr);
-    if(pos) {
-        printf("found the string '%s' in '%s' at position: %ld\n", substr, str, pos - str);
-    } else {
-        printf("the string '%s' was not found in '%s'\n", substr, str);
-    }
+void find_str(char const *str, char const *substr) {
+  const char *pos = _strstr(str, substr);
+  if (pos) {
+    printf("found the string '%s' in '%s' at position: %ld\n", substr, str,
+           pos - str);
+  } else {
+    printf("the string '%s' was not found in '%s'\n", substr, str);
+  }
 }
- 
+
 int main(void) {
-    char* str = "one two three";
-    find_str(str, "two");
-    find_str(str, "");
-    find_str(str, "nine");
-    find_str(str, "n");
- 
-    return 0;
+  char *str = "one two three";
+  find_str(str, "two");
+  find_str(str, "");
+  find_str(str, "nine");
+  find_str(str, "n");
+
+  return 0;
 }
